fcc11d4f5ec9117cb288c9d07d8e0ebb87864484
christian-iuil <okoriec01@gmail.com>
2023-07-25 13:37:35 +0100

    udpated

--- lib/features/homepage/hompage.dart
+++ lib/features/homepage/hompage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
+import '../../utils/colors.dart';
 import '../HeaderDashboard/header.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,7 +15,32 @@ class HomePage extends StatelessWidget {
         SliverToBoxAdapter(
           child: SizedBox(height: 45.h, child: const DashboardHeader()),
         ),
-       
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.all(15.sp),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Transaction History',
+                    style: TextStyle(
+                        color: AppColor.kBlackColor,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'See all',
+                    style: TextStyle(
+                        color: AppColor.kGreyColor,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              )
+            ]),
+          ),
+        ),
       ],
     ));
   }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const kWhitColor = Colors.white;
   static const kBlackColor = Colors.black;
+  static const kGreyColor = Colors.grey;
   static const kGreenColor = Colors.green;
   static const kredColor = Colors.red;
   static const kBlueColor = Colors.blue;
