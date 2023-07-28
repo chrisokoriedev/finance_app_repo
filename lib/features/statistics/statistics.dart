949e7a98a4545923ad173c74b0fee3776e516e78
christian-iuil <okoriec01@gmail.com>
2023-07-28 19:41:44 +0100

    update working on chart now

--- lib/features/statistics/statistics.dart
+++ lib/features/statistics/statistics.dart
@@ -2,6 +2,7 @@ import 'package:expense_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
+import 'package:line_icons/line_icon.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import 'model/day_model.dart';
@@ -19,21 +20,21 @@ class Statistics extends ConsumerWidget {
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Gap(5.h),
-                Text(
-                  'Statistics',
-                  style: TextStyle(
-                    fontSize: 18.sp,
-                    color: AppColor.kBlackColor,
-                    fontWeight: FontWeight.w700,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.w),
+              child: Column(
+                children: [
+                  Gap(5.h),
+                  Text(
+                    'Statistics',
+                    style: TextStyle(
+                      fontSize: 17.sp,
+                      color: AppColor.kBlackColor,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
-                ),
-                Gap(2.h),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.w),
-                  child: Row(
+                  Gap(2.h),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: List.generate(
                       4,
@@ -42,24 +43,50 @@ class Statistics extends ConsumerWidget {
                             .read(selectedTabProvider.notifier)
                             .state = index,
                         child: Chip(
+                          elevation: 0.0,
                           backgroundColor: selectedTab == index
-                              ? Colors.blue
-                              : AppColor.kBlackColor,
+                              ? AppColor.kBlackColor
+                              : AppColor.kWhitColor,
                           label: Text(
                             dayType[index],
                             style: TextStyle(
                               fontSize: 13.sp,
                               letterSpacing: 1.7,
                               fontWeight: FontWeight.w500,
-                              color: AppColor.kWhitColor,
+                              color: selectedTab == index
+                                  ? AppColor.kWhitColor
+                                  : AppColor.kBlackColor,
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Gap(1.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Chip(
+                        label: Row(
+                          children: [
+                            Text(
+                              'Expense',
+                              style: TextStyle(
+                                fontSize: 14.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Gap(2.w),
+                            LineIcon.arrowDown(
+                              size: 15.px,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ],
