6563efe4058eb4278c400154a9c7f1ed406c29e9
christian-iuil <okoriec01@gmail.com>
2023-07-29 15:16:11 +0100

    added to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:expense_app/model/create_expense.dart';
 import 'package:expense_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-
 import 'utils/routes.dart';
 
 final nameProvider = Provider((ref) => 'Chris');
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(CreateExpenseModelAdapter());
+  await Hive.openBox<CreateExpenseModel>('data');
   runApp(const ProviderScope(child: MyApp()));
 }
 
