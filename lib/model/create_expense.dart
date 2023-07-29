a49d2d6f12a8f5b3c601999257328c591c5b0876
christian-iuil <okoriec01@gmail.com>
2023-07-29 15:12:50 +0100

    create hive field

---
+++ lib/model/create_expense.dart
@@ -0,0 +1,18 @@
+import 'package:hive/hive.dart';
+part 'create_expense.g.dart';
+
+@HiveType(typeId: 1)
+class CreateExpenseModel extends HiveObject {
+  @HiveField(0)
+  String name;
+  @HiveField(1)
+  String amount;
+  @HiveField(2)
+  String expenseType;
+  @HiveField(3)
+  String explain;
+  @HiveField(4)
+  DateTime dateTime;
+  CreateExpenseModel(
+      this.name, this.amount, this.expenseType, this.explain, this.dateTime);
+}
