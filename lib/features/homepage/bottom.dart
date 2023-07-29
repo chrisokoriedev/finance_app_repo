05b168d4be2e3125c580f3188761e36d7482ebaa
christian-iuil <okoriec01@gmail.com>
2023-07-29 08:09:45 +0100

    added floating action button

--- lib/features/homepage/bottom.dart
+++ lib/features/homepage/bottom.dart
@@ -1,10 +1,23 @@
+import 'package:expense_app/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class BottomComponent extends StatelessWidget {
   const BottomComponent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColor.kGreyColor,
+        child: LineIcon.plus(
+          color: AppColor.kWhitColor,
+          size: 18.sp,
+        ),
+        onPressed: () {},
+      ),
+    );
   }
 }
