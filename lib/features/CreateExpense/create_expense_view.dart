8b4a8a61e079a98376c121dd8f11eea2b5c24ee8
christian-iuil <okoriec01@gmail.com>
2023-07-29 11:33:11 +0100

    working on ui done with the two background color

--- lib/features/CreateExpense/create_expense_view.dart
+++ lib/features/CreateExpense/create_expense_view.dart
@@ -1,10 +1,53 @@
+import 'package:expense_app/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class CreateExpenseView extends StatelessWidget {
   const CreateExpenseView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 32.h,
+                padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 10.h),
+                decoration: BoxDecoration(
+                  color: AppColor.kBlackColor,
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(30.sp),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+              top: 150,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Container(
+                  width: 80.w,
+                  height: 50.h,
+                  padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 6.h),
+                  decoration: BoxDecoration(
+                      color: AppColor.kWhitColor,
+                      borderRadius: BorderRadius.circular(10.sp),
+                      boxShadow: [
+                        BoxShadow(
+                            color: AppColor.kBlackColor.withOpacity(0.4),
+                            offset: const Offset(0, 6),
+                            blurRadius: 12,
+                            spreadRadius: 6)
+                      ]),
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
