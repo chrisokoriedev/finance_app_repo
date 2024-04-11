efac0ea7bb6efc25db890b6b439b3503f989c63b
christian-iuil <okoriec01@gmail.com>
2024-04-11 18:44:25 +0100

    update

--- lib/features/homepage/hompage.dart
+++ lib/features/homepage/hompage.dart
@@ -18,136 +18,135 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-        body: ValueListenableBuilder(
-      valueListenable: boxUse.listenable(),
-      builder:
-          (BuildContext context, Box<CreateExpenseModel> box, Widget? child) =>
-              CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: SizedBox(height: 45.h, child: const DashboardHeader()),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 3.w),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return ValueListenableBuilder(
+          valueListenable: boxUse.listenable(),
+          builder:
+      (BuildContext context, Box<CreateExpenseModel> box, Widget? child) =>
+          CustomScrollView(
+    slivers: [
+      SliverToBoxAdapter(
+        child: SizedBox(height: 45.h, child: const DashboardHeader()),
+      ),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 3.w),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Recent History',
-                          style: TextStyle(
-                              color: AppColor.kBlackColor,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        GestureDetector(
-                          onTap: pageSelected,
-                          child: Text(
-                            'See all',
-                            style: TextStyle(
-                                color: AppColor.kGreyColor,
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ),
-                      ],
+                    Text(
+                      'Recent History',
+                      style: TextStyle(
+                          color: AppColor.kBlackColor,
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w500),
                     ),
-                  ]),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: boxUse.isNotEmpty ? min(boxUse.length, 5) : 1,
-              (context, index) {
-                List<CreateExpenseModel> sortedData = boxUse.values.toList()
-                  ..sort((a, b) => b.dateTime.compareTo(a.dateTime));
-                if (boxUse.isEmpty) {
-                  return ListTile(
-                    title: Column(
-                      children: [
-                        Gap(5.h),
-                        Image.asset(
-                          'assets/gifs/coming_soon.gif',
-                          width: 70.w,
-                        ),
-                        Text(
-                          'No items to display',
-                          style: TextStyle(
-                            color: AppColor.kDarkGreyColor,
-                            fontSize: 15.sp,
-                            letterSpacing: 1.0,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-                var history = sortedData[index];
-                Icon iconData;
-                if (history.expenseType == "Income") {
-                  iconData = LineIcon.wallet(
-                    size: 18.sp,
-                    color: AppColor.kGreenColor,
-                  );
-                } else if (history.expenseType == "Expense") {
-                  iconData = LineIcon.alternateWavyMoneyBill(
-                    size: 18.sp,
-                    color: AppColor.kredColor,
-                  );
-                } else {
-                  iconData = LineIcon.alternateWavyMoneyBill(
-                    size: 18.sp,
-                    color: AppColor.kBlueColor,
-                  );
-                }
-                return ListTile(
-                  title: Row(
-                    children: [
-                      Text(
-                        '${history.expenseType}\tfor\t${history.name}',
+                    GestureDetector(
+                      onTap: pageSelected,
+                      child: Text(
+                        'See all',
                         style: TextStyle(
-                            color: AppColor.kDarkGreyColor,
-                            fontSize: 15.sp,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ],
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        history.explain,
-                        style: TextStyle(
-                            color: AppColor.kDarkGreyColor,
-                            fontSize: 13.sp,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Text(
-                        timeago.format(history.dateTime),
-                        style: TextStyle(
-                            color: AppColor.kGreyColor.shade500,
-                            fontSize: 12.sp,
+                            color: AppColor.kGreyColor,
+                            fontSize: 16.sp,
                             fontWeight: FontWeight.w500),
                       ),
-                    ],
-                  ),
-                  leading: iconData,
-                  trailing: Text(
-                    history.amount.toString(),
-                    style:
-                        TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+                    ),
+                  ],
+                ),
+              ]),
+        ),
       ),
-    ));
+      SliverList(
+        delegate: SliverChildBuilderDelegate(
+          childCount: boxUse.isNotEmpty ? min(boxUse.length, 5) : 1,
+          (context, index) {
+            List<CreateExpenseModel> sortedData = boxUse.values.toList()
+              ..sort((a, b) => b.dateTime.compareTo(a.dateTime));
+            if (boxUse.isEmpty) {
+              return ListTile(
+                title: Column(
+                  children: [
+                    Gap(5.h),
+                    Image.asset(
+                      'assets/gifs/coming_soon.gif',
+                      width: 70.w,
+                    ),
+                    Text(
+                      'No items to display',
+                      style: TextStyle(
+                        color: AppColor.kDarkGreyColor,
+                        fontSize: 15.sp,
+                        letterSpacing: 1.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            var history = sortedData[index];
+            Icon iconData;
+            if (history.expenseType == "Income") {
+              iconData = LineIcon.wallet(
+                size: 18.sp,
+                color: AppColor.kGreenColor,
+              );
+            } else if (history.expenseType == "Expense") {
+              iconData = LineIcon.alternateWavyMoneyBill(
+                size: 18.sp,
+                color: AppColor.kredColor,
+              );
+            } else {
+              iconData = LineIcon.alternateWavyMoneyBill(
+                size: 18.sp,
+                color: AppColor.kBlueColor,
+              );
+            }
+            return ListTile(
+              title: Row(
+                children: [
+                  Text(
+                    '${history.expenseType}\tfor\t${history.name}',
+                    style: TextStyle(
+                        color: AppColor.kDarkGreyColor,
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    history.explain,
+                    style: TextStyle(
+                        color: AppColor.kDarkGreyColor,
+                        fontSize: 13.sp,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    timeago.format(history.dateTime),
+                    style: TextStyle(
+                        color: AppColor.kGreyColor.shade500,
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              leading: iconData,
+              trailing: Text(
+                history.amount.toString(),
+                style:
+                    TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600),
+              ),
+            );
+          },
+        ),
+      ),
+    ],
+          ),
+        );
   }
 }
--- lib/features/statistics/statistics.dart
+++ lib/features/statistics/statistics.dart
@@ -1,3 +1,4 @@
+import 'package:expense_app/provider/item_provider.dart';
 import 'package:expense_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,103 +17,102 @@ class Statistics extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedTab = ref.watch(selectedTabProvider);
-
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 3.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Gap(5.h),
-                  Center(
-                    child: Text(
-                      'Statistics',
-                      style: TextStyle(
-                        fontSize: 17.sp,
-                        color: AppColor.kBlackColor,
-                        fontWeight: FontWeight.w700,
-                      ),
+    final itemProvider = ref.watch(itemsProvider);
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 3.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gap(5.h),
+                Center(
+                  child: Text(
+                    'Statistics',
+                    style: TextStyle(
+                      fontSize: 17.sp,
+                      color: AppColor.kBlackColor,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
-                  Gap(2.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: List.generate(
-                      4,
-                      (index) => GestureDetector(
-                        onTap: () => ref
-                            .read(selectedTabProvider.notifier)
-                            .state = index,
-                        child: Chip(
-                          elevation: 0.0,
-                          backgroundColor: selectedTab == index
-                              ? AppColor.kBlackColor
-                              : AppColor.kWhitColor,
-                          label: Text(
-                            dayType[index],
-                            style: TextStyle(
-                              fontSize: 13.sp,
-                              letterSpacing: 1.7,
-                              fontWeight: FontWeight.w500,
-                              color: selectedTab == index
-                                  ? AppColor.kWhitColor
-                                  : AppColor.kBlackColor,
-                            ),
+                ),
+                Gap(2.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(
+                    4,
+                    (index) => GestureDetector(
+                      onTap: () => ref
+                          .read(selectedTabProvider.notifier)
+                          .state = index,
+                      child: Chip(
+                        elevation: 0.0,
+                        backgroundColor: selectedTab == index
+                            ? AppColor.kBlackColor
+                            : AppColor.kWhitColor,
+                        label: Text(
+                          dayType[index],
+                          style: TextStyle(
+                            fontSize: 13.sp,
+                            letterSpacing: 1.7,
+                            fontWeight: FontWeight.w500,
+                            color: selectedTab == index
+                                ? AppColor.kWhitColor
+                                : AppColor.kBlackColor,
                           ),
                         ),
                       ),
                     ),
                   ),
-                  Gap(1.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Chip(
-                        label: Row(
-                          children: [
-                            Text(
-                              'Expense',
-                              style: TextStyle(
-                                fontSize: 15.sp,
-                                fontWeight: FontWeight.bold,
-                              ),
+                ),
+                Gap(1.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Chip(
+                      label: Row(
+                        children: [
+                          Text(
+                            'Expense',
+                            style: TextStyle(
+                              fontSize: 15.sp,
+                              fontWeight: FontWeight.bold,
                             ),
-                            Gap(2.w),
-                            LineIcon.arrowDown(
-                              size: 15.px,
-                            )
-                          ],
-                        ),
+                          ),
+                          Gap(2.w),
+                          LineIcon.arrowDown(
+                            size: 15.px,
+                          )
+                        ],
                       ),
-                    ],
-                  ),
-                  Gap(2.5.h),
-                  const ChartComponent(),
-                  Gap(2.5.h),
-                  Row(
-                    children: [
-                      Text(
-                        'Top Spending',
-                        style: TextStyle(
-                          fontSize: 14.sp,
-                          color: AppColor.kBlackColor,
-                          fontWeight: FontWeight.w700,
-                        ),
+                    ),
+                  ],
+                ),
+                Gap(2.5.h),
+                const ChartComponent(),
+                Gap(2.5.h),
+                Row(
+                  children: [
+                    Text(
+                      'Top Spending',
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        color: AppColor.kBlackColor,
+                        fontWeight: FontWeight.w700,
                       ),
-                      Gap(1.w),
-                      LineIcon.wallet(size: 16.sp,)
-                    ],
-                  ),
-                 
-                ],
-              ),
+                    ),
+                    Gap(1.w),
+                    LineIcon.wallet(
+                      size: 16.sp,
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
