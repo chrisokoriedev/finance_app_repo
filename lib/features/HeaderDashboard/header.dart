baf1ddf5a8d5eda832232ba97acfa4f2d3ba4a19
christian-iuil <okoriec01@gmail.com>
2024-04-11 02:54:04 +0100

    update

--- lib/features/HeaderDashboard/header.dart
+++ lib/features/HeaderDashboard/header.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
+
 import 'package:expense_app/model/cal_model.dart';
 import 'package:expense_app/utils/colors.dart';
 import 'package:expense_app/utils/const.dart';
