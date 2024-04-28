49524660570e7ae131ebceb477344d1a44f1da83
christian-iuil <okoriec01@gmail.com>
2024-04-28 13:26:26 +0100

    update

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -1,9 +1,11 @@
 import 'package:expense_app/domain/auth.dart';
+import 'package:expense_app/provider/local_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:local_auth/local_auth.dart';
 import 'firebase.dart';
 
-final authDataSourceProvider = StateProvider<AuthDataSource>(
-    (ref) => AuthDataSource(ref.read(firebaseAuthProvider), ref));
+final authDataSourceProvider = StateProvider<AuthDataSource>((ref) =>
+    AuthDataSource(ref.read(firebaseAuthProvider), ref,
+        ref.watch(sharedPreferencesProvider)));
 final localAuthProvider =
     Provider<LocalAuthentication>((ref) => LocalAuthentication());
